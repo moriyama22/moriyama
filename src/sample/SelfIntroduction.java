b60e26906e69d8c521ff1f8e250a7045c485a07b
amatuka <amatuka@DESKTOP-9PC4LVP>
2023-05-29 23:07:46 +0900

    課題1.3新規

---
+++ src/sample/SelfIntroduction.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+				//名前
+				String name = "森山　滉基" ;
+				//年齢
+				int age = 25 ;
+				//趣味
+				String hobby = "ゲーム" ;		
+				/*
+				 * 自己紹介文を出力する処理
+				 */
+				System.out.println("私の名前は" + name + "です。");
+				System.out.println("年齢は" + age + "歳です。");
+				System.out.println("趣味は" + hobby + "です。");
+	}
+}
