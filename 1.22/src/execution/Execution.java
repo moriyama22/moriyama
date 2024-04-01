6c002a1757791b07e5312484dbbe8fbbd9c1856f
USER <USER@LAPTOP-P2BB5GVV>
2024-04-01 15:51:44 +0900

    課題1.22

---
+++ 1.22/src/execution/Execution.java
@@ -0,0 +1,10 @@
+package execution;
+
+import processing.Processing; // 処理用のクラスをインポート
+
+public class Execution {
+    public static void main(String[] args) {
+        Processing processing = new Processing(); // Processingクラスのインスタンスを作成
+        processing.printMessages(); // メッセージを出力するメソッドを呼び出す
+    }
+}
---
+++ 1.22/src/processing/Processing.java
@@ -0,0 +1,24 @@
+package processing;
+import java.time.LocalDateTime; // 日付と時刻を扱うためのクラスをインポート
+import java.time.format.DateTimeFormatter;//日付と時刻のフォーマットを扱うためのクラスをインポート
+
+public class Processing {
+    public void printMessages() {
+        // メッセージを出力
+        System.out.println("こんにちは！ここは日本です！");
+        System.out.println("この寿司はうまい");
+        System.out.println("寿司は和食です");
+        System.out.println("今の現在日時は" + getCurrentDateTime() + "です");
+    }
+
+    private String getCurrentDateTime() {
+        // 現在の日時を取得
+        LocalDateTime currentDateTime = LocalDateTime.now(); 
+        
+        //日時を指定された形式でフォーマットする
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        String formattedDateTime = currentDateTime.format(formatter);
+        
+        return formattedDateTime; // フォーマットされた日時を返す
+    }
+}
