f92fff967878514fe4bdad5ee18101452a0dd602
USER <USER@LAPTOP-P2BB5GVV>
2024-04-02 11:11:10 +0900

    課題1.23修正

--- 1.23/src/status/Status.java
+++ 1.23/src/status/Status.java
@@ -12,43 +12,49 @@
 package status;
 
 public class Status {
-	private String name;//名前
-	private double height;//体長
-	private int speed;//速度
-	
-	//コンストラクタ（オブジェクトの初期化）
-	public Status() {
+    private String name;//名前
+    private double height;//体長
+    private int speed;//速度
+    
+    //コンストラクタ（オブジェクトの初期化）
+    public Status() {
     }
-	public Status(String name,double height,int speed) {
-		this.name = name;
-		this.height = height;
-		this.speed = speed;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public double getHeight() {
-		return height;
-	}
-	public void setHeight(double height) {
-		this.height = height;
-	}
-	public int getSpeed() {
-		return speed;
-	}
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
-	
-	// コンソールに出力するメソッド
+    
+    public Status(String name, double height, int speed) {
+        setName(name); // setNameメソッドを使用して名前をセット
+        setHeight(height); // setHeightメソッドを使用して体長をセット
+        setSpeed(speed); // setSpeedメソッドを使用して速度をセット
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public double getHeight() {
+        return height;
+    }
+    
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    
+    public int getSpeed() {
+        return speed;
+    }
+    
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    
+    // コンソールに出力するメソッド
     public void printStatus() {
-        System.out.println("動物名：" + this.name);
-        System.out.println("体長：" + this.height + "m");
-        System.out.println("速度：" + this.speed + "km/h");
+        System.out.println("動物名：" + getName()); // getName()メソッドを呼び出して名前を取得
+        System.out.println("体長：" + getHeight() + "m"); // getHeight()メソッドを呼び出して体長を取得
+        System.out.println("速度：" + getSpeed() + "km/h"); // getSpeed()メソッドを呼び出して速度を取得
     }
     
     public static void main(String[] args) {
